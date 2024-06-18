a3edf52b9a6f5e3a4e33987db6146d079d8ebba7
mai <74494945+chan-mai@users.noreply.github.com>
2024-06-18 13:03:24 +0900

    fix: fail時にもファイルを消去するように

--- src/backup.sh
+++ src/backup.sh
@@ -11,9 +11,7 @@ rclone copy --s3-upload-cutoff=5000M --multi-thread-cutoff 5000M $COMPRESSED bac
 
 # 成功確認
 if [ $? -eq 0 ]; then
-    # バックアップファイルを削除
     echo "Backup succeeded" >> /var/log/cron.log
-    rm $BACKUP_FILE $COMPRESSED
     # 成功通知
     if [ -n "$NOTIFICATION" ]; then
         curl -X POST -F content="✅バックアップが完了しました。(${COMPRESSED})" ${DISCORD_WEBHOOK_URL} &> /dev/null
@@ -26,3 +24,7 @@ else
         curl -X POST -F content="❌バックアップに失敗しました。ログを確認してください。" ${DISCORD_WEBHOOK_URL} &> /dev/null
     fi
 fi
+
+# バックアップファイルを削除
+rm -rf $BACKUP_FILE
+rm -rf $COMPRESSED
