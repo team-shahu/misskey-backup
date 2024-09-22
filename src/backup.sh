6ddfc7cb8f83dcdc1a8ee35a21ff77bb98d3ef41
mai <74494945+chan-mai@users.noreply.github.com>
2024-09-22 21:47:27 +0900

    fix backup.sh

--- src/backup.sh
+++ src/backup.sh
@@ -15,25 +15,24 @@ if [ $? -eq 0 ]; then
     # 成功通知
     if [ -n "$NOTIFICATION" ]; then
         curl -H "Content-Type: application/json" \
-        -X POST \
-        -d @- <<EOS
-        {
-                "embeds": [
-                    {
-                    "title": "✅バックアップが完了しました。",
-                    "description": "PostgreSQLのバックアップが正常に完了しました",
-                    "color": 5620992,
-                    "fields": [
+             -X POST \
+             -d '{
+                    "embeds": [
                         {
-                        "name": ":file_folder: 保存先",
-                        "value": "${COMPRESSED}",
-                        "inline":true
+                            "title": "✅バックアップが完了しました。",
+                            "description": "PostgreSQLのバックアップが正常に完了しました",
+                            "color": 5620992,
+                            "fields": [
+                                {
+                                    "name": ":file_folder: 保存先",
+                                    "value": "'"${COMPRESSED}"'",
+                                    "inline": true
+                                }
+                            ]
                         }
                     ]
-                    }
-                ]
-            } 
-        EOS ${DISCORD_WEBHOOK_URL} &> /dev/null
+                  }' \
+             "${DISCORD_WEBHOOK_URL}" &> /dev/null
     fi
 else
     # 失敗時
@@ -41,25 +40,24 @@ else
     # 通知設定の有無を確認
     if [ -n "$NOTIFICATION" ]; then
         curl -H "Content-Type: application/json" \
-        -X POST \
-        -d @- <<EOS
-        {
-            "embeds": [
-                {
-                "title": "❌バックアップに失敗しました。",
-                "description": "PostgreSQLのバックアップが異常終了しました。ログを確認してください。",
-                "color": 15548997,
-                "fields": [
-                    {
-                    "name": ":file_folder: 保存先",
-                    "value": "${COMPRESSED}",
-                    "inline":true
-                    }
-                ]
-                }
-            ]
-        }
-        EOS ${DISCORD_WEBHOOK_URL} &> /dev/null
+             -X POST \
+             -d '{
+                    "embeds": [
+                        {
+                            "title": "❌バックアップに失敗しました。",
+                            "description": "PostgreSQLのバックアップが異常終了しました。ログを確認してください。",
+                            "color": 15548997,
+                            "fields": [
+                                {
+                                    "name": ":file_folder: 保存先",
+                                    "value": "'"${COMPRESSED}"'",
+                                    "inline": true
+                                }
+                            ]
+                        }
+                    ]
+                  }' \
+             "${DISCORD_WEBHOOK_URL}" &> /dev/null
     fi
 fi
 
